72d5513fa92e9a4f0fe38a5ec104328c47549f1d
Haiyang S. Wang <30588030+astro-seanwhy@users.noreply.github.com>
2023-03-27 23:08:49 +0200

    Delete exoconstraints.py

--- pyExoInt/exoconstraints.py
+++
@@ -1,9 +0,0 @@
-from exomodel import exomodel
-
-# get pandas DataFrame of results
-result_df = exomodel('stellar_abu_files/examplestar.txt',
-                     Nsim = 1e4,
-                     refsolar = 'A21',
-                     )
-
-print(result_df)
